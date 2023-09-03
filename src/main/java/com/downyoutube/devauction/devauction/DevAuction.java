abf141846c9413dc13edd4e217c24fb51efea8a6
downYotube2548 <maneeohooho3790@gmail.com>
2023-09-03 10:17:22 +0700

    -

---
+++ src/main/java/com/downyoutube/devauction/devauction/DevAuction.java
@@ -0,0 +1,11 @@
+package com.downyoutube.devauction.devauction;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DevAuction extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
